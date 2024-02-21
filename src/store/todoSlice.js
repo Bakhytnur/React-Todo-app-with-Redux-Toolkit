1656fd9b1ff8f4d7bec8d38240003ece1cb960ed
Bakhytnur <b.s.gabdullin@gmail.com>
2024-02-21 23:09:32 +0600

    first commit

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -11,8 +11,13 @@ const todoSlice = createSlice({
             text: action.payload.text,
             completed: false,
           })},
-        removeTodo(state,action) {},
-        toggleTodoComplete(state,action) {}
+        removeTodo(state,action) {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload.id);
+        },
+        toggleTodoComplete(state,action) {
+            const toggledTodo = state.todos.find(todo => todo.id === action.payload.id);
+            toggledTodo.completed = !toggledTodo.completed;
+        }
     },
 });
 
